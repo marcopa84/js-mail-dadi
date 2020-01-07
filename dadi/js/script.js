a99ee43380fcdd166853f27d2c9ee7e90498849c
Marco <marcopa84@hotmail.it>
2020-01-07 17:05:48 +0100

    ESERCIZIO DADI FUNZIONANTE!

---
+++ dadi/js/script.js
@@ -0,0 +1,15 @@
+var utente1 = Math.floor(Math.random() * 7 + 1);
+console.log(utente1);
+var utente2 = Math.floor(Math.random() * 7 + 1);
+console.log(utente2);
+
+var message = ('Pareggio');
+
+if ( utente1 > utente2 ) {
+  message = ('Utente1 ha VINTO!');
+}
+else if ( utente2 > utente1 ) {
+  message = ('Utente2 ha VINTO!');
+}
+
+console.log(message);
