123cf561d930215ce572ccd18997a2cb122bbca9
Marco <marcopa84@hotmail.it>
2020-01-07 16:54:10 +0100

    FIX ESERCIZIO FUNZIONANTE!

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,6 +1,5 @@
 var emails = ['ciao@ciao.it', 'info@info.it', 'admin@admin.it', 'marcopa84@hotmail.it'];
 
-
 var inputEmail = prompt('Inserisci la tua Mail');
 console.log(inputEmail);
 
@@ -8,8 +7,9 @@ var message = ('NON SEI AUTORIZZATO AD ENTRARE');
 
 for (var i = 0; i < emails.length; i++){
   console.log(emails);
-  if (inputEmail == emails){
-    message = ('EMAIL AUTORIZZATA AD ENTRARE')
+  var email = emails[i]
+  if (inputEmail == email){
+    message = ('EMAIL AUTORIZZATA AD ENTRARE');
   }
 };
 
